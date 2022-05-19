8091bfeecb6b0c1006caee4c804a4721e0f9d3bd
NitishKumarGundapu <nitishkumargundapu793@gmail.com>
2022-05-19 14:19:04 +0530

    Updates

---
+++ precache-manifest.6a7504460f4dc8fbe4fd4965b3386bc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "404f9e0abbf663e97e9f",
+    "url": "/4-2-chatbot/static/css/main.5e1fbd09.chunk.css"
+  },
+  {
+    "revision": "404f9e0abbf663e97e9f",
+    "url": "/4-2-chatbot/static/js/main.404f9e0a.chunk.js"
+  },
+  {
+    "revision": "58435b267aaaf43f1cee",
+    "url": "/4-2-chatbot/static/css/1.4e3aa472.chunk.css"
+  },
+  {
+    "revision": "58435b267aaaf43f1cee",
+    "url": "/4-2-chatbot/static/js/1.58435b26.chunk.js"
+  },
+  {
+    "revision": "070daad0ba4ed77a5dde",
+    "url": "/4-2-chatbot/static/js/runtime~main.070daad0.js"
+  },
+  {
+    "revision": "b97d0bdcea3af70be660c4daae74b1dd",
+    "url": "/4-2-chatbot/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.b21ffd6ddbf80d5022cf168ad434e780.js"
+  "/4-2-chatbot/precache-manifest.6a7504460f4dc8fbe4fd4965b3386bc1.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/4-2-chatbot/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });

568a07d175d345f28a9924d65c2caae6d2aa999e
NitishKumarGundapu <nitishkumargundapu793@gmail.com>
2022-05-19 14:05:39 +0530

    Updates

---
+++ precache-manifest.9a2a49a41082f8d0ebcc6071626112db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "404f9e0abbf663e97e9f",
+    "url": "./static/css/main.5e1fbd09.chunk.css"
+  },
+  {
+    "revision": "404f9e0abbf663e97e9f",
+    "url": "./static/js/main.404f9e0a.chunk.js"
+  },
+  {
+    "revision": "c56a626b2fbfab3d5aee",
+    "url": "./static/css/1.4e3aa472.chunk.css"
+  },
+  {
+    "revision": "c56a626b2fbfab3d5aee",
+    "url": "./static/js/1.c56a626b.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "765c258d19e338ff7e1761beda9f0699",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/4-2-chatbot/precache-manifest.bfe7b5fb10c528f993a60bc0da9094df.js"
+  "./precache-manifest.9a2a49a41082f8d0ebcc6071626112db.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/4-2-chatbot/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });

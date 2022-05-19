55808b5c61fb6fa3573d732c7fc881b43e9c4f8e
NitishKumarGundapu <nitishkumargundapu793@gmail.com>
2022-05-19 13:50:05 +0530

    Updates

---
+++ precache-manifest.bfe7b5fb10c528f993a60bc0da9094df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9694aed140b552ebc383",
+    "url": "/4-2-chatbot/static/css/main.5e1fbd09.chunk.css"
+  },
+  {
+    "revision": "9694aed140b552ebc383",
+    "url": "/4-2-chatbot/static/js/main.9694aed1.chunk.js"
+  },
+  {
+    "revision": "5f87056f2983b67b55f4",
+    "url": "/4-2-chatbot/static/css/1.4e3aa472.chunk.css"
+  },
+  {
+    "revision": "5f87056f2983b67b55f4",
+    "url": "/4-2-chatbot/static/js/1.5f87056f.chunk.js"
+  },
+  {
+    "revision": "070daad0ba4ed77a5dde",
+    "url": "/4-2-chatbot/static/js/runtime~main.070daad0.js"
+  },
+  {
+    "revision": "1893b8fa691e9d69dcad9198276ccfc3",
+    "url": "/4-2-chatbot/index.html"
+  }
+];

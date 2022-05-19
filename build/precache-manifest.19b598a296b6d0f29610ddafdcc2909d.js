d3aa05f95fb02d94e59e8dddb65953f7e2bc06c8
NitishKumarGundapu <nitishkumargundapu793@gmail.com>
2022-05-19 12:56:00 +0530

    nicer

---
+++ build/precache-manifest.19b598a296b6d0f29610ddafdcc2909d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "77db3b2b24fbf25c3e8d",
+    "url": "/4-2-chatbot/static/css/main.4123e846.chunk.css"
+  },
+  {
+    "revision": "77db3b2b24fbf25c3e8d",
+    "url": "/4-2-chatbot/static/js/main.77db3b2b.chunk.js"
+  },
+  {
+    "revision": "2759a94f1ea5ae27979b",
+    "url": "/4-2-chatbot/static/css/1.d228958f.chunk.css"
+  },
+  {
+    "revision": "2759a94f1ea5ae27979b",
+    "url": "/4-2-chatbot/static/js/1.2759a94f.chunk.js"
+  },
+  {
+    "revision": "070daad0ba4ed77a5dde",
+    "url": "/4-2-chatbot/static/js/runtime~main.070daad0.js"
+  },
+  {
+    "revision": "07acb615b2e58b02f0d9ec048bffc082",
+    "url": "/4-2-chatbot/index.html"
+  }
+];
